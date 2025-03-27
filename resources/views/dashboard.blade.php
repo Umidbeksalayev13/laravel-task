4902cff980981e6f735077ed439516343ea09ea2
umidbeksalayev <umidbeksalayev13@gmail.com>
2025-03-27 21:23:18 +0500

    creating only 1 application a day is done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,6 +47,18 @@
 
                     @elseif(auth()->user()->role->name == 'client')
 
+                        @if(session()->has('error'))
+                            <div class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+                                <svg class="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                                </svg>
+                                <span class="sr-only">Info</span>
+                                <div>
+                                    <span class="font-medium">!</span> {{session()->get('error')}}
+                                </div>
+                            </div>
+                        @endif
+
                         <div class='flex items-center '>
                             <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
                                 <div class='max-w-md mx-auto space-y-6'>
